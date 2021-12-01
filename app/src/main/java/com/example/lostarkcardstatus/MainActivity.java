1d31e25253bd7ce72c7d30043da8fbafd71f17ac
ulmu0426 <kcor24@naver.com>
2021-12-01 20:08:38 +0900

    error. i need drawerlayout. i should be redesign to main screen

--- app/src/main/java/com/example/lostarkcardstatus/MainActivity.java
+++ app/src/main/java/com/example/lostarkcardstatus/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.lostarkcardstatus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sub_main);
+        setContentView(R.layout.main_page);
+
+        /*
+        ImageView imgBtnMenu;
+
+        imgBtnMenu = (ImageView) findViewById(R.id.imgBtnMenu);
+        imgBtnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerlayoutSideMenu);
+                if(!drawer.isDrawerOpen(Gravity.LEFT)){
+                    drawer.openDrawer(Gravity.LEFT);
+                }
+            }
+        });
+        */
     }
+
+
 }
