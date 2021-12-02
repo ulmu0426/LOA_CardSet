d7b46e0c4667eae46be1398d4ea4cca55b1beb74
ulmu0426 <kcor24@naver.com>
2021-12-02 20:27:37 +0900

    drawerlayout clear.

--- app/src/main/java/com/example/lostarkcardstatus/MainActivity.java
+++ app/src/main/java/com/example/lostarkcardstatus/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_page);
+        setContentView(R.layout.draw_main);
 
         /*
         ImageView imgBtnMenu;
