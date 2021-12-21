68d0d23d845b9afe4b1a1e8981d7830d5dac2263
ulmu0426 <kcor24@naver.com>
2021-12-21 19:55:57 +0900

    DB적용 작업 1일차. 미리 만든 db를 적용하는 방법 찾는 중

--- app/src/main/java/com/example/lostarkcardstatus/LOA_Card_DB.java
+++ app/src/main/java/com/example/lostarkcardstatus/LOA_Card_DB.java
@@ -4,13 +4,25 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class LOA_Card_DB extends SQLiteOpenHelper {
     private Context context;
     private static final String DATABASE_NAME = "loaCardDb.db";
     private static final int DATABASE_VERSION =1;
+    //assets 폴더
+    private static String DB_PATH = "";
+
+    private SQLiteDatabase mDataBase;
+
     //카드 테이블 6개
     private static final String TABLE_CARD_LEGEND = "card_legend";
     private static final String TABLE_CARD_EPIC = "card_epic";
@@ -46,7 +58,50 @@ public class LOA_Card_DB extends SQLiteOpenHelper {
 
     public LOA_Card_DB(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.context =context;
+        if(android.os.Build.VERSION.SDK_INT >= 17){
+            DB_PATH = context.getApplicationInfo().dataDir + "/databases/";
+        }
+        else{
+            DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        }
+
+        this.context = context;
+    }
+
+    public void createDataBase() throws IOException{
+        //데이터베이스가 없으면 assets폴더에서 복사
+        boolean mDataBaseExist = checkDataBase();
+        if(!mDataBaseExist){
+            this.getReadableDatabase();
+            this.close();
+            try {
+                copyDataBase();
+                Log.e("Log", "createDatabase database created");
+            }
+            catch (IOException mIOExeption){
+                throw new Error("ErrorCopyingDataBase");
+            }
+        }
+
+    }
+    private boolean checkDataBase(){
+        File dbFile = new File(DB_PATH + DATABASE_NAME);
+
+        return dbFile.exists();
+    }
+    //assets 폴더에서 데이터베이스 복사
+    private void copyDataBase() throws IOException{
+        InputStream mInput = context.getAssets().open(DATABASE_NAME);
+        String outFileName = DB_PATH + DATABASE_NAME;
+        OutputStream mOutput = new FileOutputStream(outFileName);
+        byte[] mBuffer = new byte[1024];
+        int mLength;
+        while ((mLength = mInput.read(mBuffer))>0){
+            mOutput.write(mBuffer,0,mLength);
+        }
+        mOutput.flush();
+        mOutput.close();
+        mInput.close();
     }
 
 
@@ -58,5 +113,19 @@ public class LOA_Card_DB extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+
+    //UPDATE 카드 수량 수정
+    public void UpdateInfoCardNum(String tableName, String number, int input, int cardId){
+        SQLiteDatabase updateColumInfo = getWritableDatabase();
+        //카드 id 값으로 카드를 파악하고 해당 카드의 수량 조절.
+        updateColumInfo.execSQL("UPDATE " + tableName + " SET " + number + " = " + input + " WHERE id = " + cardId);
+    }
+    //UPDATE 카드 각성도 수정
+    public void UpdateInfoCardAwake(String tableName, String number, int input, int cardId){
+        SQLiteDatabase updateColumInfo = getWritableDatabase();
+        //카드 id 값으로 카드를 파악하고 해당 카드의 각성도 조절.
+        updateColumInfo.execSQL("UPDATE " + tableName + " SET " + number + " = " + input + " WHERE id = " + cardId);
     }
 }
